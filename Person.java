7c709c4866c7be7ae8cebdc26a83cffb38634ff0
Mwambu Kaumba <71637268+Mwambu1@users.noreply.github.com>
2023-03-22 11:05:29 +0200

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person {
+  String name;
+  int id;
+  String address;
+  int contactNumber;
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setName(int id) {
+    this.id = id;
+  }
+  public void setName(String address) {
+    this.address = address;
+  }
+  
+  
+}
